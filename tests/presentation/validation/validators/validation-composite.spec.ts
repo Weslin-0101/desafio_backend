4d0d07d730641c4111770cf810a14a50695280e9
Wesley Lira <wesleylirac1@gmail.com>
2023-03-09 13:14:32 -0300

    test: ensure ValidationComposite returns the first error if more then one validation fails

--- tests/presentation/validation/validators/validation-composite.spec.ts
+++ tests/presentation/validation/validators/validation-composite.spec.ts
@@ -25,4 +25,12 @@ describe("ValidationComposite", () => {
     const error = sut.validate({ [field]: "any_value" });
     expect(error).toEqual(validationSpies[1].error);
   });
+
+  test("Should return the first error if more then one validation fails", () => {
+    const { sut, validationSpies } = makeSut();
+    validationSpies[0].error = new Error();
+    validationSpies[1].error = new MissingParamError(field);
+    const error = sut.validate({ [field]: "any_value" });
+    expect(error).toEqual(validationSpies[0].error);
+  });
 });
