17b6a3c016c21e8af7d825a429b3e92f0f9c9d6d
Wesley Lira <wesleylirac1@gmail.com>
2023-03-09 12:21:33 -0300

    feat: ensure FormController calls Validation with correct value

--- src/presentation/controller/form/form-controller.ts
+++ src/presentation/controller/form/form-controller.ts
@@ -1,13 +1,21 @@
 import { AddForm } from "@/domain/usecases/add-form";
-import { Controller, HttpResponse } from "@/presentation/protocols";
-import { forbidden, ok, serverError } from "@/presentation/helpes";
+import { Controller, HttpResponse, Validation } from "@/presentation/protocols";
+import { badRequest, forbidden, ok, serverError } from "@/presentation/helpes";
 import { EmailInUseError } from "@/presentation/errors";
 
 export class FormController implements Controller {
-  constructor(private readonly _addForm: AddForm) {}
+  constructor(
+    private readonly _addForm: AddForm,
+    private readonly _validation: Validation
+  ) {}
 
   async handle(request: FormController.Request): Promise<HttpResponse> {
     try {
+      const error = this._validation.validate(request);
+      if (error) {
+        return badRequest(error);
+      }
+
       const { name, email, cpf, phone } = request;
       const form = await this._addForm.add({
         name,
