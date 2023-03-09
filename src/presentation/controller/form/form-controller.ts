2bc5bac8e323127cae294a1e12509698987bda18
Wesley Lira <wesleylirac1@gmail.com>
2023-03-09 12:01:26 -0300

    feat: ensure FormController returns 500 if AddForm throws

--- src/presentation/controller/form/form-controller.ts
+++ src/presentation/controller/form/form-controller.ts
@@ -1,16 +1,19 @@
 import { AddForm } from "@/domain/usecases/add-form";
 import { Controller, HttpResponse } from "@/presentation/protocols";
+import { serverError } from "@/presentation/helpes";
 
 export class FormController implements Controller {
   constructor(private readonly _addForm: AddForm) {}
 
   async handle(request: FormController.Request): Promise<HttpResponse> {
-    await this._addForm.add(request);
-
-    return {
-      statusCode: 200,
-      body: null,
-    };
+    try {
+      await this._addForm.add(request);
+      return {
+        statusCode: 200,
+      };
+    } catch (error) {
+      return serverError(error);
+    }
   }
 }
 
