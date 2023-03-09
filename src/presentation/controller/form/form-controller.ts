860d2b805d6f73e219013b6310b01c4976515d76
Wesley Lira <wesleylirac1@gmail.com>
2023-03-09 11:50:44 -0300

    feat: add protocols in the FormContorller

--- src/presentation/controller/form/form-controller.ts
+++ src/presentation/controller/form/form-controller.ts
@@ -1,10 +1,16 @@
 import { AddForm } from "@/domain/usecases/add-form";
+import { Controller, HttpResponse } from "@/presentation/protocols";
 
-export class FormController {
+export class FormController implements Controller {
   constructor(private readonly _addForm: AddForm) {}
 
-  async handle(request: FormController.Request): Promise<void> {
+  async handle(request: FormController.Request): Promise<HttpResponse> {
     await this._addForm.add(request);
+
+    return {
+      statusCode: 200,
+      body: null,
+    };
   }
 }
 
