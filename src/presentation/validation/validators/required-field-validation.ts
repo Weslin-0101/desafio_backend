febaf1ba6819256bf0eafa7092c431d9c8009464
Wesley Lira <wesleylirac1@gmail.com>
2023-03-09 13:05:28 -0300

    feat: ensure RequiredFieldValidation returns a MissingParamError if validation fails

---
+++ src/presentation/validation/validators/required-field-validation.ts
@@ -0,0 +1,12 @@
+import { MissingParamError } from "@/presentation/errors";
+import { Validation } from "@/presentation/protocols";
+
+export class RequiredFieldValidation implements Validation {
+  constructor(private readonly _fieldName: string) {}
+
+  validate(input: any): Error {
+    if (!input[this._fieldName]) {
+      return new MissingParamError(this._fieldName);
+    }
+  }
+}
