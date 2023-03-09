cfc1ef73299d40e4940718b759f3d465a9e12d9c
Wesley Lira <wesleylirac1@gmail.com>
2023-03-09 12:44:40 -0300

    feat: ensure DbAddForm returns null if CheckFormByEmailRepository returns true

--- src/data/usecases/db-add-form.ts
+++ src/data/usecases/db-add-form.ts
@@ -1,10 +1,22 @@
 import { AddForm } from "@/domain/usecases/add-form";
 import { AddFormRepository } from "@/data/db/add-form-repository";
+import { CheckFormEmailRepository } from "../db/check-form-email-respository";
 
 export class DbAddForm implements AddForm {
-  constructor(private readonly _addFormRepository: AddFormRepository) {}
+  constructor(
+    private readonly _addFormRepository: AddFormRepository,
+    private readonly _checkFormByEmailRepository: CheckFormEmailRepository
+  ) {}
 
   async add(form: AddForm.Params): Promise<AddForm.Result> {
-    return this._addFormRepository.add(form);
+    const exists = await this._checkFormByEmailRepository.checkByEmail(
+      form.email
+    );
+
+    if (!exists) {
+      return this._addFormRepository.add(form);
+    }
+
+    return null;
   }
 }
