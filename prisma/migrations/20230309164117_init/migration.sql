1bfbd19692a481cacdf585432ebcc380b8e263b7
Wesley Lira <wesleylirac1@gmail.com>
2023-03-09 13:42:07 -0300

    feat: add Form model database

---
+++ prisma/migrations/20230309164117_init/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE "Form" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "cpf" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Form_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Form_email_key" ON "Form"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Form_cpf_key" ON "Form"("cpf");
