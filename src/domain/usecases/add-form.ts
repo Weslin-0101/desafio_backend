3a0440801dc6552b9399c83abb07aae4061c691e
Wesley Lira <wesleylirac1@gmail.com>
2023-03-08 23:00:06 -0300

    feat: add AddForm usecase

---
+++ src/domain/usecases/add-form.ts
@@ -0,0 +1,14 @@
+export interface AddForm {
+  add(form: AddForm.Params): Promise<AddForm.Result>;
+}
+
+export namespace AddForm {
+  export type Params = {
+    name: string;
+    email: string;
+    cpf: string;
+    phone: string;
+  };
+
+  export type Result = boolean;
+}
